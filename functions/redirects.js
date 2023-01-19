ea022a829682ecc2d09d4cb03103d17c20106aed
ybabenkoJapp <ybabenko@jappware.com>
2023-01-19 12:04:03 +0200

    feat(redirects): create a new filer for testing redirects

---
+++ functions/redirects.js
@@ -0,0 +1,12 @@
+exports.handler = async function (event, context) {
+  const redirectUrl = 'https://google.com'
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: redirectUrl,
+      'Cache-Control': 'no-cache',
+    },
+    body: JSON.stringify({})
+  }
+}
